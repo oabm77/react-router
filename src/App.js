73ad010f97b26b9c42e1d2ade0dbb27a44580f54
Omar Augusto Bautista Mora <oabm77@gmail.com>
2023-08-21 02:04:30 -0500

    01. Router - Inicio del problema

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
+import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
 
 function App() {
+
+  const location = window.location.pathname
+  console.log(location)
+
+  const Router = () => {
+    if (location === "/") {
+      return <Home />
+    } else {
+      return <Sobre />
+    }
+  }
+
   return (
     <>
-
+      {Router()}
     </>
   );
 }
