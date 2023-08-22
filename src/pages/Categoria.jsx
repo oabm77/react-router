5eed5fa5571d4375eacd95732629af5499f43cb4
Omar Augusto Bautista Mora <oabm77@gmail.com>
2023-08-22 01:28:47 -0500

    05 Rutas anidadas - Creando subcategorías

--- src/pages/Categoria.jsx
+++ src/pages/Categoria.jsx
@@ -3,16 +3,38 @@ import "../assets/css/blog.css"
 import { buscar } from "../api/api";
 import ListCategories from "../components/ListCategories";
 import ListPosts from "../components/ListPosts";
-import { useParams, Routes, Route } from "react-router-dom";
+import { useParams, Routes, Route, Link, useResolvedPath } from "react-router-dom";
 
 const Categoria = () => {
+    const [subcategorias, setSubcategorias] = useState([])
     const { id } = useParams()
+
+    const url = useResolvedPath("").pathname
+    console.log(url)
+
+    useEffect(() => {
+        buscar(`/categorias?id=${id}`, (response) => {
+            setSubcategorias(response[0].subcategorias)
+        })
+    }, [id])
+
     return (
         <>
             <div className="container">
                 <h2 className="title-page">Pet Noticias</h2>
             </div>
             <ListCategories />
+            <ul className="category-list container flex">
+                {
+                    subcategorias.map(subcategoria => (
+                        <li className={`category-list__category category-list__category--${id}`} key={subcategoria} >
+                            <Link to={`${url}/${subcategoria}`}>
+                                {subcategoria}
+                            </Link>
+                        </li>
+                    ))
+                }
+            </ul>
             <Routes>
                 <Route path="/" element={<ListPosts url={`/posts?categoria=${id}`} />}/>
             </Routes>
